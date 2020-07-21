de6a6da7505a3fc08a3e419a839a0411c2785e79
ckielasjensen <ckielasjensen@uiowa.edu>
2020-07-21 12:50:28 -0500

    Created a utility module which currently only has one function which is used to save figures. Added the new ATC example figures.

---
+++ Examples/utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 11:47:09 2020
+
+@author: ckielasjensen
+"""
+import os
+
+import matplotlib.pyplot as plt
+
+
+def saveFigs(figDir='Figures', figFormat='svg'):
+    # Create a Figures directory if it doesn't already exist
+    if not os.path.isdir(figDir):
+        os.mkdir(figDir)
+
+    for i in plt.get_fignums():
+        fig = plt.figure(i)
+        ax = fig.get_axes()[0]
+        title = ax.get_title()
+        if title == '':
+            print('[!] Warning: No figure title, figure will be saved without a name.')
+        print(f'Saving figure {i} - {title}')
+
+        ax.set_title('')
+        plt.tight_layout()
+        plt.draw()
+        saveName = os.path.join(figDir, title.replace(' ', '_') + '.' + figFormat)
+        fig.savefig(saveName, format=figFormat)
+        ax.set_title(title)
+        plt.draw()
+
+    print('Done saving figures')
+
+
+if __name__ == '__main__':
+    pass
