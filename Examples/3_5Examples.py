e6d1a734b6ef8536288fe7bc4b6e060f489a9f89
ckielasjensen <ckielasjensen@uiowa.edu>
2020-07-21 16:32:03 -0500

    Added the 3.5 examples along with their corresponding plots. Slightly modified the rational BP object.

---
+++ Examples/3_5Examples.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 10:54:57 2020
+
+@author: ckielasjensen
+"""
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import numpy as np
+from scipy.spatial import ConvexHull
+
+from constants import DEG_ELEV
+from polynomial.bernstein import Bernstein
+from polynomial.rationalbernstein import RationalBernstein
+
+
+SAVE_FIG = True         # Set to True to save figures
+FIG_FORMAT = 'svg'      # Used for the output format when saving figures
+FIG_DIR = 'Figures/3_5'     # Directory in which to save the figures
+XLIM = [-0.5, 12.5]
+YLIM = [-0.5, 12.5]
+
+
+def setRCParams():
+    # Run this to make sure that the matplotlib plots have the correct font type
+    # for an IEEE publication. Also sets font sizes and line widths for easier
+    # viewing.
+    plt.rcParams.update({
+                'font.size': 24,
+                'figure.titlesize': 40,
+                'pdf.fonttype': 42,
+                'ps.fonttype': 42,
+                'xtick.labelsize': 40,
+                'ytick.labelsize': 40,
+                'lines.linewidth': 4,
+                'lines.markersize': 18,
+                'figure.figsize': [13.333, 10]
+                })
+    # plt.tight_layout()
+
+
+def resetRCParams():
+    # Reset the matplotlib parameters
+    plt.rcParams.update(plt.rcParamsDefault)
+
+
+def initialPlot(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    c1.plot(ax, color='C0', label='c1')
+    c2.plot(ax, color='C1', label='c2')
+    ax.add_patch(obs)
+
+    ax.set_title('Initial Figure')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_xlim(XLIM)
+    ax.set_ylim(YLIM)
+    ax.legend()
+
+
+def endPoints(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    c1.plot(ax, showCpts=False, color='C0')
+    c2.plot(ax, showCpts=False, color='C1')
+    ax.add_patch(obs)
+
+    for i, pt in enumerate(np.concatenate([c1.cpts[:, (0, -1)].T, c2.cpts[:, (0, -1)].T])):
+        ax.plot(pt[0], pt[1], 'k.')
+
+        # This if statement is purely for modifying the position of the drawn coordinates
+        if i % 2:
+            ax.text(pt[0]-3.5, pt[1]-1, f'({pt[0]}, {pt[1]})')
+        else:
+            ax.text(pt[0], pt[1], f'({pt[0]}, {pt[1]})')
+
+    ax.set_title('End Points')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_xlim(XLIM)
+    ax.set_ylim(YLIM)
+
+
+def convexHull(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    for cpts in [c1.cpts, c2.cpts]:
+        plotCvxHull(cpts, ax)
+
+    c1.plot(ax, color='C0')
+    c2.plot(ax, color='C1')
+    ax.add_patch(obs)
+
+    ax.set_title('Convex Hull')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_xlim(XLIM)
+    ax.set_ylim(YLIM)
+
+
+def convexHullSplit(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    c1L, c1R = c1.split(0.5*(c1.tf-c1.t0)+c1.t0)
+    c2L, c2R = c2.split(0.5*(c2.tf-c2.t0)+c2.t0)
+
+    c1L.plot(ax, color='C0')
+    c1R.plot(ax, color='C2')
+    c2L.plot(ax, color='C1')
+    c2R.plot(ax, color='C3')
+
+    for cpts in [c1L.cpts, c1R.cpts, c2L.cpts, c2R.cpts]:
+        plotCvxHull(cpts, ax)
+
+    ax.add_patch(obs)
+    ax.set_title('Split Convex Hull')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_xlim(XLIM)
+    ax.set_ylim(YLIM)
+
+
+def convexHullElev(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    c1E = c1.elev(10)
+    c2E = c2.elev(10)
+
+    c1E.plot(ax, color='C0')
+    c2E.plot(ax, color='C1')
+
+    for cpts in [c1E.cpts, c2E.cpts]:
+        plotCvxHull(cpts, ax)
+
+    ax.add_patch(obs)
+    ax.set_title('Elevated Convex Hull')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_xlim(XLIM)
+    ax.set_ylim(YLIM)
+
+
+def speedSquared(c1, c2):
+    fig, ax = plt.subplots()
+
+    c1speed = c1.diff().normSquare()
+    c2speed = c2.diff().normSquare()
+
+    c1speed.plot(ax, showCpts=False, color='C0', label='c1 speed')
+    c2speed.plot(ax, showCpts=False, color='C1', label='c2 speed')
+
+    ax.plot([c1speed.t0, c1speed.tf], [c1speed.min()]*2, 'r--', label='c1 minimum speed')
+    ax.plot([c2speed.t0, c2speed.tf], [c2speed.min()]*2, 'b--', label='c2 minimum speed')
+
+    ax.set_title('Speed Squared')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel(r'Squared Speed $\left( \frac{m}{s} \right)^2$')
+    ax.legend()
+
+
+def headingAngle(c1, c2):
+    fig, ax = plt.subplots()
+
+    c1dot = c1.diff()
+    c2dot = c2.diff()
+
+    c1tan = c1dot.y / c1dot.x
+    c2tan = c2dot.y / c2dot.x
+
+    c1tan.plot(ax, showCpts=False, color='C0', label='c1 tangent')
+    c2tan.plot(ax, showCpts=False, color='C1', label='c2 tangent')
+
+    ax.plot([c1tan.t0, c1tan.tf], [c1tan.min()]*2, 'r--', label='c1 minimum tangent')
+    ax.plot([c2tan.t0, c1tan.tf], [c2tan.min()]*2, 'b--', label='c2 minimum tangent')
+
+    ax.set_title('Tangent of Heading Angle')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel(r'$\tan (\psi)$')
+    ax.legend()
+
+
+def angularRate(c1, c2):
+    fig, ax = plt.subplots()
+
+    angrate1 = _angularRate(c1)
+    angrate2 = _angularRate(c2)
+
+
+    cpts1 = np.concatenate([[np.linspace(angrate1.t0, angrate1.tf, angrate1.deg+1)], angrate1.cpts])
+    cpts2 = np.concatenate([[np.linspace(angrate2.t0, angrate2.tf, angrate2.deg+1)], angrate2.cpts])
+
+    angrate1.plot(ax, showCpts=False, color='C0', label='c1 angular rate')
+    angrate2.plot(ax, showCpts=False, color='C1', label='c2 angular rate')
+
+    ax.plot([angrate1.t0, angrate1.tf], [angrate1.min()]*2, 'r--', label='c1 minimum angular rate')
+    ax.plot([angrate2.t0, angrate2.tf], [angrate2.min()]*2, 'b--', label='c2 minimum angular rate')
+
+    ax.set_title('Angular Rate')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel(r'Angular Rate $\left( \frac{rad}{s^2} \right)$')
+    ax.legend()
+
+
+def distSqr(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    obsPoly = Bernstein(np.atleast_2d(obs.center).T*np.ones((2, c1.deg+1)), min(c1.t0, c2.t0), max(c1.tf, c2.tf))
+
+    c1c2 = c1 - c2
+    c1obs = c1 - obsPoly
+    c2obs = c2 - obsPoly
+
+    c1c2.normSquare().plot(ax, label='c1 to c2')
+    c1obs.normSquare().plot(ax, label='c1 to obstacle')
+    c2obs.normSquare().plot(ax, label='c2 to obstacle')
+
+    ax.set_title('Squared Distance Between Trajectories and Obstacle', wrap=True)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel(r'Squared Distance $(m^2)$')
+    ax.legend()
+
+
+def minDist(c1, c2, obs):
+    fig, ax = plt.subplots()
+
+    c1.plot(ax, showCpts=False, color='C0', label='c1')
+    c2.plot(ax, showCpts=False, color='C1', label='c2')
+
+    dist, t1, t2 = c1.minDist(c2)
+
+    pt1 = c1(t1)
+    pt2 = c2(t2)
+
+    ax.plot([pt1[0], pt2[0]], [pt1[1], pt2[1]], 'k-', label='Minimum distance line')
+    mid = pt1 + 0.5*(pt2-pt1)
+    ax.text(mid[0], mid[1], f'Minimum Distance: {dist}')
+
+    ax.set_title('Minimum Spatial Distance')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.legend()
+
+
+def _angularRate(bp):
+    bpdot = bp.diff()
+    bpddot = bpdot.diff()
+
+    xdot = bpdot.x
+    ydot = bpdot.y
+    xddot = bpddot.x
+    yddot = bpddot.y
+
+    num = yddot*xdot - xddot*ydot
+    den = xdot*xdot + ydot*ydot
+
+    cpts = num.elev(DEG_ELEV).cpts / den.elev(DEG_ELEV).cpts
+    wgts = den.elev(DEG_ELEV).cpts
+    # cpts = num.cpts / den.cpts
+    # wgts = den.cpts
+
+    return RationalBernstein(cpts, wgts, bp.t0, bp.tf)
+
+
+def plotCvxHull(cpts, ax):
+    hull = ConvexHull(cpts.T)
+    for simplex in hull.simplices:
+        ax.plot(cpts[0, simplex], cpts[1, simplex], 'k:')
+
+
+def saveFigs():
+    import os
+    # Create a Figures directory if it doesn't already exist
+    if not os.path.isdir(FIG_DIR):
+        os.mkdir(FIG_DIR)
+
+    for i in plt.get_fignums():
+        fig = plt.figure(i)
+        ax = fig.get_axes()[0]
+        title = ax.get_title()
+        print(f'Saving figure {i} - {title}')
+
+        ax.set_title('')
+        plt.tight_layout()
+        plt.draw()
+        saveName = os.path.join(FIG_DIR, title.replace(' ', '_') + '.' + FIG_FORMAT)
+        fig.savefig(saveName, format=FIG_FORMAT)
+        ax.set_title(title)
+        plt.draw()
+
+    print('Done saving figures')
+
+
+if __name__ == '__main__':
+    plt.close('all')
+    setRCParams()
+
+    # Rectangle obstacle
+    def obs(): return Circle((3, 4), 1, ec='k', lw=4)
+    # Control points
+    cpts1 = np.array([[0, 2, 4, 6, 8, 10],
+                      [5, 0, 2, 3, 10, 3]], dtype=float)
+    cpts2 = np.array([[1, 3, 6, 8, 10, 12],
+                      [6, 9, 10, 11, 8, 8]], dtype=float)
+    # Bernstein polynomials
+    c1 = Bernstein(cpts1, t0=10, tf=20)
+    c2 = Bernstein(cpts2, t0=10, tf=20)
+
+    # initialPlot(c1, c2, obs())
+    # endPoints(c1, c2, obs())
+    # convexHull(c1, c2, obs())
+    # convexHullSplit(c1, c2, obs())
+    # convexHullElev(c1, c2, obs())
+    speedSquared(c1, c2)
+    headingAngle(c1, c2)
+    angularRate(c1, c2)
+    minDist(c1, c2, obs())
+    # distSqr(c1, c2, obs())
+
+    plt.show()
+
+    if SAVE_FIG:
+        saveFigs()
+
+    resetRCParams()
--- polynomial/rationalbernstein.py
+++ polynomial/rationalbernstein.py
@@ -51,6 +51,96 @@ class RationalBernstein(Base):
 
         return self._curve
 
+    def min(self, dim=0, globMin=np.inf, tol=1e-6):
+        """Returns the minimum value of the Bernstein polynomialin a single
+        dimension
+
+        Finds the minimum value of the Bernstein polynomial. This is done by
+        first checking the first and last control points since the first and
+        last point lie on the curve. If the first or last control point is not
+        the minimum value, the curve is split at the lowest control point. The
+        new minimum value is then defined as the lowest control point of the
+        two new curves. This continues until the difference between the new
+        minimum and old minimum values is within the desired tolerance.
+
+        :param dim: Which dimension to return the minimum of.
+        :type dim: int
+        :param tol: Tolerance of the minimum value.
+        :type tol: float
+        :param maxIter: Maximum number of iterations to search for the minimum.
+        :type maxIter: int
+        :return: Minimum value of the Bernstein polynomial. None if maximum
+            iterations is met.
+        :rtype: float or None
+        """
+        ub = self.cpts[dim, (0, -1)].min()
+        if ub < globMin:
+            globMin = ub
+
+        minIdx = self.cpts[dim, :].argmin()
+        lb = self.cpts[dim, minIdx]
+
+        # Prune if the global min is less than the lower bound
+        if globMin < lb:
+            return globMin
+
+        # If we are within the desired tolerance, return
+        if ub - lb < tol:
+            return globMin
+
+        # Otherwise split and continue
+        else:
+            tdiv = (minIdx/self.deg)*(self.tf - self.t0) + self.t0
+            c1, c2 = self.split(tdiv)
+            c1min = c1.min(dim=dim, globMin=globMin, tol=tol)
+            c2min = c2.min(dim=dim, globMin=globMin, tol=tol)
+
+            return min(c1min, c2min)
+
+    def split(self, tDiv):
+        """Splits the curve into two curves at point tDiv
+
+        Note that the two returned curves will have the SAME tf value as the
+        original curve. This may result in slightly unexpected behavior for a
+        1D curve when plotting since both slices of the original curve will
+        also be plotted on [0, tf]. The behavior should work as expected when
+        plotting in 2D though.
+
+        Paper Reference: Property 5: The de Casteljau Algorithm
+
+        :param tDiv: Point at which to split the curve
+        :type tDiv: float
+        :return: Tuple of curves. One before the split point and one after.
+        :rtype: tuple(Bernstein, Bernstein)
+        """
+        # c1 = self.copy()
+        # c2 = self.copy()
+
+        cpts1 = []
+        cpts2 = []
+
+        if np.isnan(tDiv):
+            print('[!] Warning, tDiv is {}, changing to 0.'.format(tDiv))
+            tDiv = 0
+
+        for d in range(self.dim):
+            left, right = deCasteljauSplit(self.cpts[d, :], tDiv - self.t0,
+                                           self.tf - self.t0)
+            cpts1.append([left])
+            cpts2.append([right[::-1]])
+
+        # c1.cpts = cpts1
+        # c1.t0 = self.t0
+        # c1.tf = tDiv
+        # c2.cpts = cpts2
+        # c2.t0 = tDiv
+        # c2.tf = self.tf
+
+        c1 = RationalBernstein(cpts=np.concatenate(cpts1), weights=self._wgts, t0=self.t0, tf=tDiv)
+        c2 = RationalBernstein(cpts=np.concatenate(cpts2), weights=self._wgts, t0=tDiv, tf=self.tf)
+
+        return c1, c2
+
 
 def _ratBernPoly(cpts, wgts, npts):
     dim = cpts.shape[0]
@@ -103,6 +193,51 @@ def _bernBasisMat(n):
             b[i, k] = (-1)**(i-k) * binom(n, i) * binom(i, k)
 
     return b
+
+
+@njit(cache=True)
+def deCasteljauSplit(cpts, tDiv, tf=1.0):
+    """Uses the de Casteljau algorithm to split the curve at tDiv
+
+    This function is similar to the de Casteljau curve function but instead of
+    drawing a curve, it returns two sets of control points which define the
+    curve to the left and to the right of the split point, tDiv.
+
+    Paper Reference: Property 5: The de Casteljau Algorithm
+
+    :param cpts: Control points defining the 1D Bernstein polynomial.
+    :type cpts: numpy.ndarray(dtype=numpy.float64)
+    :param tDiv: Point at which to divide the curve.
+    :type tDiv: float
+    :param tf: Final tau value for the 1D curve. Default is 1.0. Note that the
+        Bernstein polynomial is defined on the range of [0, tf].
+    :type tf: float
+    :return: Returns a tuple of numpy arrays. The zeroth element is the
+        control points defining the curve to the left of tDiv. The first
+        element is the control points defining the curve to the right of tDiv.
+    :rtype: tuple(numpy.ndarray, numpy.ndarray)
+    """
+    tDiv = tDiv/tf
+    cptsLeft = np.zeros(cpts.size)
+    cptsRight = np.zeros(cpts.size)
+    idx = 0
+
+    newCpts = cpts.copy()
+    cptsTemp = cpts.copy()
+    while newCpts.size > 1:
+        cptsLeft[idx] = cptsTemp[0]
+        cptsRight[idx] = cptsTemp[-1]
+        idx += 1
+
+        cptsTemp = np.empty(newCpts.size-1)
+        for i in range(cptsTemp.size):
+            cptsTemp[i] = (1-tDiv)*newCpts[i] + tDiv*newCpts[i+1]
+
+        newCpts = cptsTemp.copy()
+
+    cptsLeft[-1] = cptsRight[-1] = newCpts[0]
+
+    return cptsLeft, cptsRight
 
 
 if __name__ == '__main__':
